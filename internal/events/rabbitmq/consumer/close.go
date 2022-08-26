8f3edd81f692968ba2d3c69f24687e5d9471d942
Fernando Bandeira <fernando.bandeira94@gmail.com>
2022-08-26 02:52:40 -0300

    temporary fix for the graceful consumer shutdown

--- internal/events/rabbitmq/consumer/close.go
+++ internal/events/rabbitmq/consumer/close.go
@@ -1,18 +1,22 @@
 package consumer
 
+import "fmt"
+
 // Graceful shutdown of the consumer.
-func (r rabbitmqConsumer) Close() error {
+func (r *rabbitmqConsumer) Close() error {
 	r.logger.Info().Msg("closing consumer")
 
-	// First we close the channel manager
-	// This also stops the consumer from receiving new messages
+	// TODO stop consuming messages first
+	// We'll block till all the active go routines are done
+	fmt.Printf("waiting on waitgroup\n")
+	r.wg.Wait()
+
+	// Now we'll close the channel
 	err := r.chManager.Close()
 	if err != nil {
 		return err
 	}
 
-	// Now we'll block till all the active go routines are done
-	r.wg.Wait()
 	r.logger.Info().Msg("consumer closed gracefully")
 	return nil
 }
