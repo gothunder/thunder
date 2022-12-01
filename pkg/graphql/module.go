dce2504ed319be5daca49bbd378f3e8561f480a0
Fernando Bandeira <fernando.bandeira94@gmail.com>
2022-12-01 17:42:59 -0300

    fix graphql handler

--- pkg/graphql/module.go
+++ pkg/graphql/module.go
@@ -1,12 +1,11 @@
 package graphql
 
 import (
-	"github.com/gothunder/thunder/internal/graphql"
 	"go.uber.org/fx"
 )
 
 var Module = fx.Options(
 	fx.Provide(
-		graphql.NewGraphqlHandler,
+		newGraphqlHandler,
 	),
 )
