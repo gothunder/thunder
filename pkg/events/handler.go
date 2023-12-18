974b327f231aa31872b34590b87d6ccdf1554c8f
mactep <brunoro.tulio@gmail.com>
2023-12-17 21:01:21 -0300

    Upstream TheRafaBonin's event helper

--- pkg/events/handler.go
+++ pkg/events/handler.go
@@ -1,6 +1,11 @@
 package events
 
-import "context"
+import (
+	"context"
+	"regexp"
+
+	"github.com/TheRafaBonin/roxy"
+)
 
 type HandlerResponse int
 
@@ -29,4 +34,46 @@ type Handler interface {
 
 	// The function that will be called when a subscribed message is received.
 	Handle(ctx context.Context, topic string, decoder EventDecoder) HandlerResponse
+}
+
+/*
+MatchTopicAndFormatsMessage matches the topic and formats the message.
+
+It takes the following parameters:
+   - ctx: the context.Context object for the function.
+   - decoder: the thunderEvents.EventDecoder object for decoding the message.
+   - referenceTopic: the reference topic string for matching.
+   - topic: the topic string to match against the reference topic.
+   - message: the message object to be formatted.
+
+It returns a pointer to the formatted message object and any error encountered.
+if the match fails, nil is returned.
+*/
+func MatchTopicAndFormatsMessage[T interface{}](
+	ctx context.Context,
+	decoder EventDecoder,
+	referenceTopic string,
+	topic string,
+	message T,
+) (*T, error) {
+	// Declare some variables
+	var match bool
+	var err error
+
+	match, err = regexp.MatchString(referenceTopic, topic)
+	err = roxy.Wrap(err, "Failed to match payment status topic string")
+	if err != nil {
+		return nil, err
+	}
+	if match {
+		err := decoder.Decode(&message)
+		roxy.Wrap(err, "unmarshalling message")
+		if err != nil {
+			return nil, err
+		}
+
+		return &message, nil
+	}
+
+	return nil, nil
 }
