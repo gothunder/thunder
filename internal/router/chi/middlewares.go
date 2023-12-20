821118408508255ae8599a9cc2c6c3fb0ee0dc7c
Lucas Amaral <lucas@alternative.co>
2023-12-20 14:57:34 -0300

    feat: adds request_id and request dump middlewares (#69)
    * feat: adds request_id and request dump middlewares
    
    * add requestID to aroundOperations

--- internal/router/chi/middlewares.go
+++ internal/router/chi/middlewares.go
@@ -1,10 +1,16 @@
 package chi
 
 import (
+	"bytes"
+	"fmt"
+	"io"
+	"net/http"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gothunder/thunder/internal/log"
 	"github.com/rs/zerolog"
+	zlog "github.com/rs/zerolog/log"
 )
 
 func defaultMiddlewares(r *chi.Mux, logger *zerolog.Logger) {
@@ -14,6 +20,54 @@ func defaultMiddlewares(r *chi.Mux, logger *zerolog.Logger) {
 	// Ggets sets the right RemoteAddr for the request
 	r.Use(middleware.RealIP)
 
+	// Adds a request id to the context of each request
+	r.Use(middleware.RequestID)
+
 	// Add a logger instance to the context with some default values
 	r.Use(log.Middleware(logger))
+
+	// Logs the request
+	r.Use(requestDumpMiddleware())
+}
+
+func requestDumpMiddleware() func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			var strBody string
+
+			body, err := r.GetBody()
+			if err != nil {
+				strBody = fmt.Sprintf("error getting body: %s", err.Error())
+			} else {
+				bytesBody, err := io.ReadAll(body)
+				if err != nil {
+					strBody = fmt.Sprintf("error reading body: %s", err.Error())
+				} else {
+					strBody = string(bytesBody)
+				}
+			}
+
+			logger := zlog.Ctx(r.Context())
+
+			headerBuffer := bytes.Buffer{}
+			var strHeader string
+
+			err = r.Header.Write(&headerBuffer)
+			if err != nil {
+				strHeader = fmt.Sprintf("error writing headers to string: %s", err.Error())
+			} else {
+				strHeader = headerBuffer.String()
+			}
+
+			logger.Debug().
+				Str("headers", strHeader).
+				Str("body", strBody).
+				Str("requestID", middleware.GetReqID(r.Context())).
+				Msg("logging request")
+
+			next.ServeHTTP(w, r)
+		})
+	}
+
 }
